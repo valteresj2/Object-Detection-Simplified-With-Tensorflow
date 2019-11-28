77d68a586138812225c911435ac95b646695d116
Valter Eduardo <valteresj2@gmail.com>
2019-11-28 16:20:50 -0300

    Update process_model.sh

--- process_model.sh
+++ process_model.sh
@@ -3,7 +3,7 @@ conda activate tensorflowlite
 python xml_to_csv.py
 
 python change_number_labels.py --file_label_name /conf_model/labelmap.pbtxt --file_conf_model /conf_model/model_conf.config
-
+mkdir datatfrecord
 python generate_tfrecord.py --csv_input=train_labels.csv --label_input conf_model/labelmap.pbtxt --image_dir=train --output_path=datatfrecord/train.record
 
 python generate_tfrecord.py --csv_input=test_labels.csv --label_input conf_model/labelmap.pbtxt --image_dir=test --output_path=datatfrecord/test.record
